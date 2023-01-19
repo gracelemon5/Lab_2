c648b4aa00d38ace05aafeba8f7911d1b071c02e
lemon5 <lemon5@X-KNOY306PC12>
2023-01-19 13:53:18 -0500

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A; -3;
+	float B; 5;
+	float X;
+	cout << "Hello, my name is Grace and I'm going to solve the equation:";
+	cout << "Ax + B = 0";
+	cin >> A;
+	cout << "A=" << A << endl;
+	B = (A * 4) + 1;
+	cout << "And B=" << B << endl;
+}
